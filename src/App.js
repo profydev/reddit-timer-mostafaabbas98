8fb183f6ad4f7f12e2454fc167db7be3800d994e
Mostafa Abbas <mostafaabbas2902@gmail.com>
2022-03-12 03:46:08 +0200

    add Header.js component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Search from './components/pages/Search';
+import Header from './Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<Search />} />
