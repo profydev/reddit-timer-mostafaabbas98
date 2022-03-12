7d19ae5c4a58ef4f38f6d50a919669c966a841ba
Mostafa Abbas <53439948+mostafaabbas98@users.noreply.github.com>
2022-03-12 20:03:05 +0200

    Create app skeleton (#1)
    * setup app and add global style
    
    * add router and main components
    
    * fix react-router error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Search from './components/pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+
+// import normalize.css to reset default style;
+import 'normalize.css';
+
+// import global styles
+import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
