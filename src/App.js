6e8e836a28a5a5a821f06c0070bb697751121b3e
Mostafa Abbas <mostafaabbas2902@gmail.com>
2022-03-12 03:29:09 +0200

    fix react-router error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Search from './components/pages/Search';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="search" element={<Search />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
